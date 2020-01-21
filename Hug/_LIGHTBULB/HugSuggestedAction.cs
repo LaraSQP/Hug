e1d1d180e009cecd58ccc4d2cb49e97a3a7d1063
LaraSQP <github@sequencepublishing.com>
2020-01-21 15:02:45 +0900

    1.0.6

--- Hug/_LIGHTBULB/HugSuggestedAction.cs
+++ Hug/_LIGHTBULB/HugSuggestedAction.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using Microsoft.VisualStudio.Imaging.Interop;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
@@ -13,12 +10,15 @@ namespace Hug
 {
 	internal class HugSuggestedAction : ISuggestedAction
 	{
+		// Constants
+		const int MenuItemLength = 50;
+
 		// Public properties
-		public bool HasActionSets => false;
+		public bool			HasActionSets => false;
 		public ImageMoniker IconMoniker => default;
-		public string IconAutomationText => null;
-		public string InputGestureText => null;
-		public bool HasPreview => false;
+		public string		IconAutomationText => null;
+		public string		InputGestureText => null;
+		public bool			HasPreview => false;
 		public string DisplayText
 		{
 			get; private set;
@@ -51,7 +51,34 @@ namespace Hug
 			TrackingSpan	= trackingSpan;
 			Snapshot		= TrackingSpan.TextBuffer.CurrentSnapshot;
 			Tagged			= Item.Left + TrackingSpan.GetText( Snapshot ) + Item.Right;
-			DisplayText		= string.Format( "Hug as {0}", Tagged );
+
+			if( Tagged.Length < MenuItemLength )
+			{
+				DisplayText = string.Format( "Hug as {0}", Tagged );
+			}
+			else
+			{
+				if( Item.Left.Length + Item.Right.Length >= MenuItemLength )
+				{
+					// The tags alone are too large, so try to fit an ellipsis
+					DisplayText = string.Format( "Hug as {0}", Item.Left + "…" + Item.Right );
+				}
+				else
+				{
+					var text		= TrackingSpan.GetText( Snapshot ).Trim( '.' );
+					var shortened	= Item.Left + text + Item.Right;
+
+					if( shortened.Length > MenuItemLength )
+					{
+						shortened = Item.Left
+									+ text.Substring( 0, text.Length - ( shortened.Length - MenuItemLength ) )
+									+ "…"
+									+ Item.Right;
+					}
+
+					DisplayText = string.Format( "Hug as {0}", shortened );
+				}
+			}
 		}
 
 
